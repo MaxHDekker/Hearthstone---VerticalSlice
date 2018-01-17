c1e6eccc70ea26aebb99f7ab3adfb5463b1d562b
gwnrobin <robinschellingerhout@live.nl>
2018-01-17 01:15:00 +0100

    add cards

--- Robin/HeartStone/Assets/YourHand.cs
+++ Robin/HeartStone/Assets/YourHand.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class YourHand : MonoBehaviour
 {
     List<Card> cardsInHand = new List<Card>();
-    public List<Card> choosableCards;
+    public List<Card> choosableCards = new List<Card>();
 
     public GameObject deck;
     private Deck cardDeck;
 
     void Start()
     {
-        choosableCards = new List<Card>();
-
         cardDeck = deck.GetComponent<Deck>();
     }
 
     public void chooseCards()
     {
-        for (int i = 0; i < choosableCards.Count; i++)
+        for (int i = 0; i < 3; i++)
         {
-            choosableCards[i] = cardDeck.GetTopDeck();
+            choosableCards.Add(cardDeck.GetTopDeck());
         }
+    }
+
+    public void ShowCard()
+    {
+
     }
 
     public void TakeCard()
