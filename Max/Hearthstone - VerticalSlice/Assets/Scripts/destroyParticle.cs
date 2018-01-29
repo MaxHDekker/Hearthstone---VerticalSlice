7c342ba534e84e9df01f962c4d20196ad0237bb6
Max Dekker <xamrekked@gmail.com>
2018-01-29 17:22:31 +0100

    All i have
    everything

---
+++ Max/Hearthstone - VerticalSlice/Assets/Scripts/destroyParticle.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyParticle : MonoBehaviour {
+
+	void Update () {
+        Destroy(gameObject, 0.7f);
+	}
+}
