fc4d727eb976d175ca83b9a8494b29cc81390100
Max Dekker <xamrekked@gmail.com>
2018-01-30 11:18:44 +0100

    Robins Gem mixed with Max gem
    2 games van max en robin gecombined

---
+++ Robin/HeartStone3.0/Assets/Scripts/ParticleDust.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDust : MonoBehaviour
+{
+
+    public ParticleSystem particle;
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "floor")
+        {
+            particle.gameObject.GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+}
